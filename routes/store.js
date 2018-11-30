278dd8b1a10a146a5d648e4f3c217ece365c67e9
alexbook00 <43623251+alexbook00@users.noreply.github.com>
2018-11-29 21:10:40 -0700

    updated after Thursday meeting

--- routes/store.js
+++ routes/store.js
@@ -7,13 +7,13 @@ app.get('/', function (request, response) {
 
     // TODO: Initialize the query variable with a SQL query
     // that returns all the rows in the ‘store’ table
-    var query = 'select * from store';
+    var query = 'select * from songs';
 
     db.any(query)
         .then(function (rows) {
             // render views/store/list.ejs template file
             response.render('store/list', {
-                title: 'Store listing',
+                title: 'Current Queue',
                 data: rows
             })
         })
@@ -21,7 +21,7 @@ app.get('/', function (request, response) {
             // display error message in case an error
             request.flash('error', err);
             response.render('store/list', {
-                title: 'Store listing',
+                title: 'Current Queue',
                 data: ''
             })
         })
@@ -30,49 +30,54 @@ app.get('/', function (request, response) {
 app.get('/add', function (request, response) {
     // render views/store/add.ejs
     response.render('store/add', {
-        title: 'Add New Item',
-        sname: '',
-        qty: '',
-        price: ''
+        title: 'Add New Song',
+        description: ''
+        //sname: '',
+        //qty: '',
+        //price: ''
     })
 });
 
 // Route to insert values. Notice that request method is POST here
 app.post('/add', function (request, response) {
     // Validate user input - ensure non emptiness
-    request.assert('sname', 'sname is required').notEmpty();
-    request.assert('qty', 'Quantity is required').notEmpty();
-    request.assert('price', 'Price is required').notEmpty();
+    request.assert('description', 'description is required').notEmpty();
+    //request.assert('sname', 'sname is required').notEmpty();
+    //request.assert('qty', 'Quantity is required').notEmpty();
+    //request.assert('price', 'Price is required').notEmpty();
 
     var errors = request.validationErrors();
     if (!errors) { // No validation errors
-        var item = {
+        var /*item*/song = {
             // sanitize() is a function used to prevent Hackers from inserting
             // malicious code(as data) into our database. There by preventing
             // SQL-injection attacks.
-            sname: request.sanitize('sname').escape().trim(),
-            qty: request.sanitize('qty').escape().trim(),
-            price: request.sanitize('price').escape().trim()
+            description: request.sanitize('description').escape().trim(),
+            //sname: request.sanitize('sname').escape().trim(),
+            //qty: request.sanitize('qty').escape().trim(),
+            //price: request.sanitize('price').escape().trim()
         };
         // Running SQL query to insert data into the store table
-        db.none('INSERT INTO store(sname, qty, price) VALUES($1, $2, $3)', [item.sname, item.qty, item.price])
+        db.none('INSERT INTO songs(description, ytlink, votes, addedwhen) VALUES($1, $2, $3, $4)', [song.description, 'use youtube video id to link to stuff Will did', 0, 'now()'/*item.sname, item.qty, item.price*/])
             .then(function (result) {
                 request.flash('success', 'Data added successfully!');
                 // render views/store/add.ejs
                 response.render('store/add', {
-                    title: 'Add New Item',
-                    sname: '',
-                    qty: '',
-                    price: ''
+                    title: 'Add New Song',
+                    description: ''
+                    //sname: '',
+                    //qty: '',
+                    //price: ''
                 })
             }).catch(function (err) {
             request.flash('error', err);
             // render views/store/add.ejs
             response.render('store/add', {
-                title: 'Add New Item',
-                sname: item.sname,
-                qty: item.qty,
-                price: item.price
+                title: 'Add New Song',
+                description: song.description
+                //sname: item.sname,
+                //qty: item.qty,
+                //price: item.price
             })
         })
     } else {
@@ -80,9 +85,10 @@ app.post('/add', function (request, response) {
         request.flash('error', error_msg);
         response.render('store/add', {
             title: 'Add New Item',
-            sname: request.body.sname,
-            qty: request.body.qty,
-            price: request.body.price
+            description: request.body.description
+            //sname: request.body.sname,
+            //qty: request.body.qty,
+            //price: request.body.price
         })
     }
 });
@@ -179,12 +185,12 @@ app.put('/edit/(:id)', function (req, res) {
 // Route to delete an item. Notice that request method is DELETE here
 app.delete('/delete/(:id)', function (req, res) {
     // Fetch item id of the item to be deleted from the request.
-    var itemId = req.params.id;
+    var songDescription = req.params.id;
 
     // TODO: Initialize the deleteQuery variable with a SQL query
     // that deletes an item whose id = itemId in the
     // 'store' table
-    var deleteQuery = 'delete from store where id = ' + itemId + ';';
+    var deleteQuery = 'delete from songs where description = ' + songDescription + ';';
     db.none(deleteQuery)
         .then(function (result) {
                   req.flash('success', 'successfully deleted it');
